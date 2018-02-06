984de6346b9e884d26ab89fcdc10f85bbdd31991
sobriodev <sobriodev@gmail.com>
2018-02-06 19:01:53 +0100

    Add 4th exercise

---
+++ chapter7/exercise4/exercise4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+long double probability(unsigned numbers, unsigned picks);
+
+int main() {
+    unsigned numbers, picks, extraNumbers;
+    std::cout << "Enter [numbers picks extraNumbers]";
+    while (std::cin >> numbers >> picks >> extraNumbers && picks <= numbers) {
+        std::cout << "Probability equals 1 : "
+                  << probability(numbers, picks) * probability(extraNumbers, 1)
+                  << std::endl;
+        std::cout << "Enter other numbers" << std::endl;
+    }
+}
+
+long double probability(unsigned numbers, unsigned picks) {
+    long double result = 1;
+    for (int i = 0; i < picks; ++i) {
+        result *= (long double) (numbers - i) / (picks - i);
+    }
+    return result;
+}
