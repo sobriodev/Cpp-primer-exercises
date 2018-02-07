05a175d916b83bd64b2e7ccab31331d90b20dcc0
sobriodev <sobriodev@gmail.com>
2018-02-07 17:54:30 +0100

    Add 5th exercise

---
+++ chapter7/exercise5/exercise5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+long factorial(unsigned num);
+
+int main() {
+    unsigned num;
+    std::cout << "Enter number" << std::endl;
+    while (std::cin >> num) {
+        std::cout << "Factorial("
+                  << num
+                  << " ) equals "
+                  << factorial(num)
+                  << std::endl;
+        std::cout << "Enter another number" << std::endl;
+    }
+}
+
+long factorial(unsigned num) {
+    if (num == 0) { return 1; }
+    return num * factorial(num - 1);
+}
